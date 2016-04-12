669d7d7afa6c55c1595f32a1382eda872db46b8e
Rodrigo Sestari <sestari@Rodrigos-MacBook-Pro.local>
2016-04-13 00:28:47 +0200

    spark sql

--- clearFile.py
+++ clearFile.py
@@ -5,7 +5,8 @@ Created on Apr 12, 2016
 '''
 from __future__ import print_function
 from pyspark import SparkContext,SparkConf
-from pyspark.sql.types import StructField,StringType
+from pyspark.sql.types import StructField,StringType,LongType
+from pyspark.sql import SQLContext
 import os
 import time
 
@@ -22,26 +23,37 @@ if __name__ == "__main__":
         #.set('spark.executor.extraClassPath', '/Users/sestari/Documents/mongo-hadoop-core-1.5.2.jar') #;/Users/sestari/Documents/mongo-java-driver-3.0.4.jar;/Users/sestari/Documents/mongodb-driver-3.2.2.jar')
 
     sc = SparkContext(conf=conf)
+    sqlContext = SQLContext(sc)
    
     log = sc.textFile("logs/p.csv")  
     
+    
     #function to get the categories through URL
     def getCategory(requestHttp):        
-        return requestHttp
+        return requestHttp.strip('"').encode('utf-8')
         
     #function to get the position through the cell
     def getLocation(cellPosition):
-        return ""
+        return cellPosition.strip('"').encode('utf-8')
  
-        
-    #it will be used in Spark SQL    
-    schemaCSV ='Ip;Resquest;3;4;Url;6;7;8;listOfURI;sequenceId;serviceContainers;12'
-    fields = [StructField(field_name, StringType(), True) for field_name in schemaCSV.split(';')]
+  
     
     #get the important fields
-    resultMap = log.map(lambda line: line.split(";")).filter(lambda line: len(line)>1).map(lambda row: (row[1], getCategory(row[5]),getLocation(row[6]),row[10] ))
+    resultMap = log.map(lambda line: line.split(";")).filter(lambda line: len(line)>1).map(lambda row: (long(row[1]), getCategory(row[4]) ,getLocation(row[5]),long(row[9]) )).repartition(1)
     
+    #sql save
+    schemaCSV ='_id;_url;_5;sequenceId'
+    fields = [StructField(field_name, StringType(), True) for field_name in schemaCSV.split(';')]
+    fields[0].dataType = LongType()
+    fields[3].dataType = LongType()
+
+
+    #schemaDataFrame= sqlContext.createDataFrame(resultMap, fields)
+    ##data = sqlContext.read.format("libsvm").save("result"+str(time.time()))
+    
+    #normal save
     resultMap.saveAsTextFile("result"+str(time.time()))
+    
     #line.saveAsNewAPIHadoopFile("/test.txt", 'org.apache.hadoop.mapreduce.lib.output.SequenceFileOutputFormat' )
 
    
--- createCluster.py
+++ createCluster.py
@@ -3,6 +3,51 @@ Created on Apr 12, 2016
 
 @author: sestari
 '''
+from __future__ import print_function
+from pyspark import SparkContext,SparkConf
+from pyspark.sql.types import StructField,StringType
+from pyspark.sql import SQLContext
+import os
 
-if __name__ == '__main__':
-    pass
+
+
+
+if __name__ == "__main__":
+
+    os.environ["SPARK_HOME"] = "/Users/sestari/Documents/spark-1.6.1-bin-hadoop2.6"
+                                #/usr/hdp/current/spark-client
+    
+    conf = SparkConf().setAppName("Spark Cluster")
+        #setMaster("spark://127.0.0.1:7077")
+        #.setSparkHome('/Users/sestari/Documents/spark-1.6.1-bin-hadoop2.6') \
+        #.set('spark.executor.extraClassPath', '/Users/sestari/Documents/mongo-hadoop-core-1.5.2.jar') #;/Users/sestari/Documents/mongo-java-driver-3.0.4.jar;/Users/sestari/Documents/mongodb-driver-3.2.2.jar')
+
+    sc = SparkContext(conf=conf)     
+    sqlContext = SQLContext(sc)
+
+   
+    logSplited = sc.textFile("result1460495650.17/part-00000")
+    data = sqlContext.read.format("libsvm").load("data/mllib/sample_libsvm_data.txt")  
+    
+    #function to get the categories through URL
+    def getCategory(requestHttp):        
+        return requestHttp
+        
+    #function to get the position through the cell
+    def getLocation(cellPosition):
+        return ""
+ 
+        
+    #create Data Frame with Spark SQL
+    schemaCSV ='Resquest;Url;6;sequenceId'
+    fields = [StructField(field_name, StringType(), True) for field_name in schemaCSV.split(';')]
+    schemaDataFrame= sqlContext.createDataFrame(logSplited, fields)
+    
+    
+    
+    
+    
+   
+    
+    sc.stop()
+    
